bb3a8419d0d0bea9970abbb706986cfe87e7466d
Marcelo Guimaraes <marceloguimaraes@outlook.com.br>
2020-08-27 23:54:51 -0300

    created connection with database

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
-/* eslint-disable no-console */
+import 'reflect-metadata';
 import express from 'express';
 import routes from './routes';
+
+import './database';
 
 const app = express();
 
@@ -9,5 +11,6 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => {
+  // eslint-disable-next-line no-console
   console.log('Server started on port 3333!');
 });
